507f502a261df8280ec0552e61a9bed7fb3418a3
V S <vsyrovat@gmail.com>
2014-11-09 16:30:43 +1000

    + Download last minor release

--- run.rb
+++ run.rb
@@ -17,38 +17,48 @@ end
 
 content1 = Net::HTTP.get('kernel.ubuntu.com', '/~kernel-ppa/mainline/')
 doc1 = Nokogiri::HTML(content1)
+versions = []
 doc1.css('body a').each do |a1|
   a1.content.strip.scan(/\Av(.*)\-[a-z]+\/?\z/) do |k1|
-    if version
-      if k1.flatten.first == version
-        url = "/~kernel-ppa/mainline/#{a1['href']}"
-        links = []
-        content2 = Net::HTTP.get('kernel.ubuntu.com', url)
-        doc2 = Nokogiri::HTML(content2)
-        doc2.css('body a').each do |a2|
-          a2['href'].strip.scan(/\Alinux-(?:image|headers)-(.*)\.deb\z/) do |k2|
-            links.push("http://kernel.ubuntu.com#{url}#{a2['href']}") if k2.first =~ /_all$/ || k2.first =~ r
-          end
+    versions.push([a1['href'], k1.flatten.first]) if k1.flatten.first
+  end
+end
+
+e = version.split('.')
+if e.pop == 'latest'
+  version = versions.select{|v| v[1] =~ Regexp.new("^#{e.join('\.')}\\.\\d+$")}.max{|v| v[1].to_i}[1]
+end
+
+if version
+  versions.each do |v|
+    if v[1] == version
+      url = "/~kernel-ppa/mainline/#{v[0]}"
+      links = []
+      content2 = Net::HTTP.get('kernel.ubuntu.com', url)
+      doc2 = Nokogiri::HTML(content2)
+      doc2.css('body a').each do |a2|
+        a2['href'].strip.scan(/\Alinux-(?:image|headers)-(.*)\.deb\z/) do |k2|
+          links.push("http://kernel.ubuntu.com#{url}#{a2['href']}") if k2.first =~ /_all$/ || k2.first =~ r
         end
-        dir = File.join(Dir.pwd, "#{distro}_#{version}_#{type}_#{architecture}")
-        links.each do |link|
-          f = link.split('/').last
-          tf = File.join(dir, f)
-          FileUtils.mkdir_p dir
-          if File.exist? tf
-            puts "#{f} already exists"
-          else
-            `axel -o "#{tf}" "#{link}"`
-            puts "Got #{f}"
-          end
-        end
-        puts "Deb files placed in #{dir}"
-        exit
       end
-    else
-      puts k1
+      dir = File.join(Dir.pwd, "#{distro}_#{version}_#{type}_#{architecture}")
+      links.each do |link|
+        f = link.split('/').last
+        tf = File.join(dir, f)
+        FileUtils.mkdir_p dir
+        if File.exist? tf
+          puts "#{f} already exists"
+        else
+          `axel -o "#{tf}" "#{link}"`
+          puts "Got #{f}"
+        end
+      end
+      puts "Deb files placed in #{dir}"
+      exit
     end
   end
+else
+  puts versions.join(', ')
 end
 
 puts "Could not find version #{version}"
